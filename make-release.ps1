2e525a811ade45df72c4791e3546ade68944a536
eteeselink <e@teeselink.nl>
2011-04-16 18:33:14 +0200

    Moved make-release.ps1 to zip instead of 7z

--- make-release.ps1
+++ make-release.ps1
@@ -1,7 +1,9 @@
 $version =  Read-Host "Enter version"
 $version = $version.Trim()
 
-& "c:\Program Files\7-Zip\7z.exe" a Releases\Mug-$version.7z Mug\bin\Release\*
+pushd Mug\bin\Release
+& "c:\Program Files\7-Zip\7z.exe" a ..\..\..\Releases\Mug-$version.zip .\*
+popd
 
 Write-Host "Done! Don't forget to git tag -a Mug-$version"
 
